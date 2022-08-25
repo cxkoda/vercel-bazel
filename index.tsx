203f1b4c632a0fc9c71a583a89e8ba18016c29fa
David Huber <dave@yomi.eu>
2022-08-26 00:33:45 +0200

    Make the div clickable

--- index.tsx
+++ index.tsx
@@ -2,7 +2,34 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as styles from "./styles.css";
 
+function Hello() {
+  const [isActive, setIsActive] = React.useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
+  console.log("Hello");
+
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: isActive ? "green" : "black",
+        }}
+        onClick={handleClick}
+      >
+        <h1 className={styles.h1} onClick={handleClick}>
+          Hello, world! asdf
+        </h1>
+      </div>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <h1 className={styles.h1}>Hello, world!</h1>,
+  <>
+    <Hello />
+  </>,
   document.getElementById("root")
 );
